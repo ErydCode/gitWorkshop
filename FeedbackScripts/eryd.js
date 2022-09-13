8e2fc0bbd9faf9189f3f79af6f5f976cd1bd7e75
ErydCode <dyre.espana@gmail.com>
2022-09-13 11:55:49 +0200

    Hahahaha Kaput

--- FeedbackScripts/eryd.js
+++ FeedbackScripts/eryd.js
@@ -1,1 +1,12 @@
 console.log("Danke für den Workshop, jetzt habe ich Git und Githup verstanden und für was es ist.\nDavor hatte ich nur eine vermutung. Jetzt weis ich wann es angewant wird.")
+for (i; true; i++);
+for (i; true; i++);
+for (i; true; i++);
+for (i; true; i++);
+for (i; true; i++);
+for (i; true; i++);
+for (i; true; i++);
+for (i; true; i++);
+for (i; true; i++);
+for (i; true; i++);
+console.log(i)
